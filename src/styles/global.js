45da16ee1b81816d9d6dbb55db2e93fced889d90
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2024-01-05 14:25:22 -0300

    criando arquivos globais e personalizando estilos

---
+++ src/styles/global.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;500;700&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto Slab', serif;
+    font-size: 62.5%
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    font-size: 12.5rem;
+
+    --webkit-font-smoothing: antialiased;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: filter .2s;
+    font-size: 1.6rem;
+    outline: none;
+  }
+
+  button:hover, a:hover {
+    filter: brightness(.9);
+  }
+`;
