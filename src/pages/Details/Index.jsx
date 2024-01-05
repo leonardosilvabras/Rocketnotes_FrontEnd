b51311d6f4f3099d8a8df31f046ae22f4643b92c
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2024-01-05 15:49:03 -0300

    component da secao

--- src/pages/Details/Index.jsx
+++ src/pages/Details/Index.jsx
@@ -1,12 +1,21 @@
-import { Container } from "./styles.js";
+import { Container, Links } from "./styles.js";
 
 import { Header } from "../../components/Header/index.jsx";
 import { Button } from "../../components/Button/index.jsx";
+import { Section } from "../../components/Section/index.jsx";
 
 export function Details() {
   return (
     <Container>
       <Header />
+
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
+          <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
+        </Links>
+      </Section>
+
       <Button title="Voltar" />
     </Container>
   );
