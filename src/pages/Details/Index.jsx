caf33243fbf4a8dcfafbf1f881a2cdde0293262e
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2024-01-05 16:11:19 -0300

    component buttonText

--- src/pages/Details/Index.jsx
+++ src/pages/Details/Index.jsx
@@ -4,20 +4,32 @@ import { Header } from "../../components/Header/index.jsx";
 import { Button } from "../../components/Button/index.jsx";
 import { Section } from "../../components/Section/index.jsx";
 import { Tag } from "../../components/Tag/index.jsx";
+import { ButtonText } from "../../components/ButtonText/index.jsx";
 
 export function Details() {
   return (
     <Container>
       <Header />
 
+      <ButtonText title="Excluir a nota"/>
+
       <Section title="Links úteis">
         <Links>
-          <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
-          <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
+          <li>
+            <a href="https://www.rocketseat.com.br/">
+              https://www.rocketseat.com.br/
+            </a>
+          </li>
+          <li>
+            <a href="https://www.rocketseat.com.br/">
+              https://www.rocketseat.com.br/
+            </a>
+          </li>
         </Links>
       </Section>
       <Section title="Marcadores">
-        <Tag title="NodeJS" />
+        <Tag title="express" />
+        <Tag title="node" />
       </Section>
 
       <Button title="Voltar" />
