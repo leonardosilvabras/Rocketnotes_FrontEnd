878e522298a27d365c50647d95527db092b0c87c
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2024-01-05 15:57:48 -0300

    component tag

--- src/pages/Details/Index.jsx
+++ src/pages/Details/Index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./styles.js";
 import { Header } from "../../components/Header/index.jsx";
 import { Button } from "../../components/Button/index.jsx";
 import { Section } from "../../components/Section/index.jsx";
+import { Tag } from "../../components/Tag/index.jsx";
 
 export function Details() {
   return (
@@ -14,6 +15,9 @@ export function Details() {
           <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
           <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
         </Links>
+      </Section>
+      <Section title="Marcadores">
+        <Tag title="NodeJS" />
       </Section>
 
       <Button title="Voltar" />
