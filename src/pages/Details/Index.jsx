66b7a0e54970c2f54cecb493df73649d3a1c5a31
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2024-01-05 16:24:40 -0300

    finalizacao interface detalhes e components

--- src/pages/Details/Index.jsx
+++ src/pages/Details/Index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from "./styles.js";
+import { Container, Links, Content } from "./styles.js";
 
 import { Header } from "../../components/Header/index.jsx";
 import { Button } from "../../components/Button/index.jsx";
@@ -11,28 +11,46 @@ export function Details() {
     <Container>
       <Header />
 
-      <ButtonText title="Excluir a nota"/>
+      <main>
+        <Content>
+          <ButtonText title="Excluir a nota" />
 
-      <Section title="Links úteis">
-        <Links>
-          <li>
-            <a href="https://www.rocketseat.com.br/">
-              https://www.rocketseat.com.br/
-            </a>
-          </li>
-          <li>
-            <a href="https://www.rocketseat.com.br/">
-              https://www.rocketseat.com.br/
-            </a>
-          </li>
-        </Links>
-      </Section>
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="node" />
-      </Section>
+          <h1>Introdução ao React</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
 
-      <Button title="Voltar" />
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="https://www.rocketseat.com.br/">
+                  https://www.rocketseat.com.br/
+                </a>
+              </li>
+              <li>
+                <a href="https://www.rocketseat.com.br/">
+                  https://www.rocketseat.com.br/
+                </a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   );
 }
