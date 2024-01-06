6e657668b1dba71932d2ddbd13e44aa56cd6a836
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2024-01-06 15:14:45 -0300

    finalizando component NoteItem

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,8 @@
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import { Textarea } from "../../components/Textarea";
+import { NoteItem } from "../../components/NoteItem";
+import { Section } from "../../components/Section";
 import { Container, Form } from "./styles";
 import { ButtonText } from "../../components/ButtonText";
 
@@ -18,6 +20,11 @@ export function New() {
 
           <Input placeholder="Título" />
           <Textarea placeholder="Observações" />
+
+          <Section title="Links úteis">
+            <NoteItem value="htpps://rocketseat.com.br" />
+            <NoteItem isNew placeholder="Novo link" />
+          </Section>
         </Form>
       </main>
     </Container>
