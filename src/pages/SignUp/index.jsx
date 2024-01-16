4ffbd01b648d290d9f98ed08d34a9bdf03bdcd79
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2024-01-16 16:01:35 -0300

    integrando server e client no register

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,12 +1,37 @@
+import { useState } from "react";
+
 import { Container, Form, Background } from "./styles";
 import { FiUser, FiLock, FiMail } from "react-icons/fi";
+
+import { api } from "../../services/api";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { ButtonText } from "../../components/ButtonText";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate()
+
+  function handleSignUp() {
+    if (!name || !email || !password) return alert("Preencha todos os campos");
+
+    api.post("/users", { name, email, password }).then(()=>{
+      alert("Usuário criado com sucesso!");
+      navigate("/")
+    }).catch(error => {
+      if(error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possivel realizar o cadastro");
+      }
+    });
+  }
+
   return (
     <Container>
       <Background />
@@ -16,10 +41,25 @@ export function SignUp() {
 
         <h2>Crie sua conta</h2>
 
-        <Input placeholder="Nome" type="text" icon={FiUser} />
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
-        <Button title="Cadastrar" />
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button title="Cadastrar" onClick={handleSignUp} />
 
         <Link to="/">
           <ButtonText title="Voltar para o login" />
