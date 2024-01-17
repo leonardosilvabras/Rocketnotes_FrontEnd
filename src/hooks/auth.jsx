e5c3d3c6cb979603bc6e7e053daf710468c9bcac
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2024-01-17 15:09:19 -0300

    localstorage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,16 +1,19 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from "../services/api";
 
 const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
-  const [data, setData] = useState("");
+  const [data, setData] = useState({});
 
   async function signIn({ email, password }) {
     try {
       const response = await api.post("/sessions", { email, password });
       const { user, token } = response.data;
+
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+      localStorage.setItem("@rocketnotes:token", token);
 
       api.defaults.headers.authorization = `Bearer ${token}`;
       setData({ user, token });
@@ -19,6 +22,17 @@ function AuthProvider({ children }) {
       alert("Nao foi possivel entrar");
     }
   }
+
+  useEffect(() => {
+    const token = localStorage.getItem("@rocketnotes:token");
+    const user = localStorage.getItem("@rocketnotes:user");
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      setData({ token, user: JSON.parse(user) });
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
