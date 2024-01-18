c7588885ec2108ef0964b52091e465a53daa32a1
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2024-01-18 14:38:40 -0300

    handleRemoveTag

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -32,6 +32,10 @@ export function New() {
     setNewTag("");
   }
 
+  function handleRemoveTag(deleted) {
+    setTags((prevState) => prevState.filter((tag => tag !== deleted)));
+  }
+
   return (
     <Container>
       <Header />
@@ -68,7 +72,11 @@ export function New() {
           <Section title="Marcadores">
             <div className="tags">
               {tags.map((tag, index) => (
-                <NoteItem key={String(index)} value={tag} onClick={() => {}} />
+                <NoteItem
+                  key={String(index)}
+                  value={tag}
+                  onClick={() => handleRemoveTag(tag)}
+                />
               ))}
               <NoteItem
                 isNew
